7b0f5928078cdc819418b0eeadaed50579bebf3a
sanaa elhafer <sanaa.elhafer@gmail.com>
2024-11-11 20:49:27 +0100

    first commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,22 @@
+
+
+const Nav = () => {
+  return (
+    <div>
+       <nav className="container">
+       <div className="log">
+       <img src="src\assets\logo4.png" alt="logo" />
+       </div>
+       <ul >
+        <li href="#">MENU</li>
+        <li href="#">LOCATION</li>
+        <li href="#">ABOUT</li>
+        <li href="#">CONTACT</li>
+       </ul>
+       <button >LOGIN</button>
+      </nav>
+    </div>
+  )
+}
+
+export default Nav;
